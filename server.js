da9eccde8e2bdf73f97201d1f625734f79dbf4c0
Sreerag o s <sreeragsnair142@gmail.com>
2024-06-25 17:39:48 +0530

    second commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+  .connect(process.env.MONGODB_URL || "", {
     useNewUrlParser: true,
   })
   .then(() => console.log("MongoDB Connected"))
