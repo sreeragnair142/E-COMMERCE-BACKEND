17b1f51c11aa4e127cd05fa4e25ee6b273dd6cb5
Sreerag o s <sreeragsnair142@gmail.com>
2024-07-01 20:53:12 +0530

    INITIAL COMMIT

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 import productRoute from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors())
 mongoose
 .set('strictQuery', true)
   .connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
